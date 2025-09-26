393553cda85f9e7a9f46b684da8cad978c0293dd
mikael <mikael.st15@protonmail>
2025-09-26 20:13:11 -0300

    adding /assets folder into react version

---
+++ react/src/assets/palette.ts
@@ -0,0 +1,16 @@
+const Palette = {
+  tool_bars: '#0B0A0F',
+  content: '#0F0E14',
+  items: '#1D1B27',
+  white: '#FFFFFF',
+  blue: '#1F4C9F',
+  light_blue: '#508CEE',
+  black: '#000000',
+  transparent: '#FF000000',
+  red: '#BA0000',
+  lightRed: '#FF5252',
+  gray: '#56526b',
+  green: '#27B72F',
+}
+
+export default Palette;
