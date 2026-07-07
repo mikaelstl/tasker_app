0a782d629e4a9dd9f3f4d8fa6cf57a3c6b524b87
mikael <mikael.st15@protonmail.com>
2026-07-07 08:10:10 -0300

    feat: finished new login screen structure

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,65 +1,123 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Field,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from "@/components/ui/field"
+} from "@/components/ui/field";
 import {
   InputGroup,
   InputGroupAddon,
   InputGroupInput,
-} from "@/components/ui/input-group"
-import { KeyRound, Mail } from "lucide-react"
+} from "@/components/ui/input-group";
+import { KeyRound, Mail } from "lucide-react";
+import { useForm } from "@tanstack/react-form";
+import * as z from "zod";
+import type { LoginDTO } from "@/service/types/auth/login.dto";
+
+interface LoginFormProps extends React.ComponentProps<"form"> {
+  login: (data: LoginDTO) => Promise<void>
+}
+
+const schema = z.object({
+  email: z.email().nonempty(),
+  password: z.string().nonempty()
+}) satisfies z.ZodType<LoginDTO>;
+
 
 export function LoginForm({
+  login,
   className,
   ...props
-}: React.ComponentProps<"form">) {
+}: LoginFormProps) {
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    validators: {
+      onSubmit: schema
+    },
+    onSubmit: async ({ value }) => await login(value)
+  })
+
+  const onSubmit = (evt: React.FormEvent) => {
+    evt.preventDefault();
+    form.handleSubmit();
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form
+      className={cn("flex flex-col gap-6", className)}
+      {...props}
+      onSubmit={onSubmit}
+    >
       <FieldGroup>
         <div className="flex flex-col items-center gap-1 text-center">
           <h1 className="text-2xl font-bold">LOGIN</h1>
         </div>
-        <Field>
-          <FieldLabel htmlFor="email">Email</FieldLabel>
-          <InputGroup align="start">
-            <InputGroupInput
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              autoComplete="email"
-              required
-            />
-            <InputGroupAddon align="start">
-              <Mail className="size-4" aria-hidden="true" />
-            </InputGroupAddon>
-          </InputGroup>
-        </Field>
-        <Field>
-          <div className="flex items-center">
-            <FieldLabel htmlFor="password">Password</FieldLabel>
-            {/* <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a> */}
-          </div>
-          <InputGroup align="start">
-            <InputGroupInput
-              id="password"
-              type="password"
-              autoComplete="current-password"
-              required
-            />
-            <InputGroupAddon align="start">
-              <KeyRound className="size-4" aria-hidden="true" />
-            </InputGroupAddon>
-          </InputGroup>
-        </Field>
+        <form.Field
+          name="email"
+          children={(field) => {
+            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                <InputGroup align="start">
+                  <InputGroupInput
+                    id={field.name}
+                    type="email"
+                    value={field.state.value as string}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    aria-invalid={isInvalid}
+                    placeholder="m@example.com"
+                    autoComplete="email"
+                    required
+                  />
+                  <InputGroupAddon align="start">
+                    <Mail className="size-4" aria-hidden="true" />
+                  </InputGroupAddon>
+                </InputGroup>
+              </Field>
+            )
+          }}
+        />
+        <form.Field
+          name="password"
+          children={(field) => {
+            const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
+            return (
+              <Field data-invalid={isInvalid}>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                  {/* <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a> */}
+                </div>
+
+                <InputGroup align="start">
+                  <InputGroupInput
+                    id={field.name}
+                    type="password"
+                    value={field.state.value as string}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    aria-invalid={isInvalid}
+                    required
+                  />
+                  <InputGroupAddon align="start">
+                    <KeyRound className="size-4" aria-hidden="true" />
+                  </InputGroupAddon>
+                </InputGroup>
+              </Field>
+            )
+          }}
+        />
         <Field>
           <Button type="submit">Login</Button>
         </Field>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,38 +1,19 @@
 import { LoginForm } from "@/components/login-form";
 import { useAuth } from "@/hooks/useAuth";
-import type { LoginDTO } from "@/service/types/auth/login.dto";
-import { useNavigate } from "react-router-dom";
-import * as z from "zod";
-import { useForm } from "@tanstack/react-form";
-import { toast } from "sonner";
 import { Logo } from "@/components/images/logo";
 import { LogoIcon } from "@/components/images/logo-icon";
-// import { useForm } from "react-hook-form";
-
-const schema = z.object({
-  email: z.email().nonempty(),
-  password: z.string().nonempty()
-}) satisfies z.ZodType<LoginDTO>;
+import type { LoginDTO } from "@/service/types/auth/login.dto";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export function Login() {
   const { login } = useAuth();
 
   const navigate = useNavigate();
 
-  const form = useForm({
-    defaultValues: {
-      email: '',
-      password: ''
-    },
-    validators: {
-      onSubmit: schema
-    },
-    onSubmit: async ({ value }) => await onLogin(value)
-  })
-
-  const onLogin = async (value: LoginDTO) => {
+  const handleLogin = async (data: LoginDTO) => {
     try {
-      await login(value);
+      await login(data);
 
       console.log("login success");
 
@@ -41,11 +22,7 @@ export function Login() {
     } catch (err) {
       console.error(err);
     }
-  }
 
-  const onSubmit = (evt: React.FormEvent) => {
-    evt.preventDefault();
-    form.handleSubmit();
   }
 
   return (
@@ -56,15 +33,13 @@ export function Login() {
       <div className="tskr-login-form-container flex flex-col gap-4 p-6 md:p-10 bg-foreground">
         <div className="tskr-login-page-icon flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <LogoIcon className="size-6"/>
+            <LogoIcon className="size-6" />
             Tasker
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <LoginForm
-              onSubmit={onSubmit}
-            />
+            <LoginForm login={handleLogin}/>
           </div>
         </div>
       </div>
